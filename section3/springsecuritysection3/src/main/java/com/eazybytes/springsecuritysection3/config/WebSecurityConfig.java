60c59e11cdc261a35e73b7873ca30bee2bb5f203
PierreSQS <nomo_messina@web.de>
2022-03-23 22:31:09 +0100

    Sec3_Chap16: initial commit

---
+++ section3/springsecuritysection3/src/main/java/com/eazybytes/springsecuritysection3/config/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.eazybytes.springsecuritysection3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * /myAccount - secured
+     * /myBalance - secured
+     * /myLoans - secured
+     * /notices - not secured
+     * /contact - not secured
+    */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/contact", "/notices").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .formLogin()
+            .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Admin")
+                .password("{noop}Admin123")
+                .authorities("ADMIN")
+             .and()
+                .withUser("Pierrot")
+                .password("{noop}Pierrot123")
+                .authorities("READ");
+    }
+}
