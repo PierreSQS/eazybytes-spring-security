aa5f83b3e737474cf4f3b78e1a76d8215a993dff
PierreSQS <nomo_messina@web.de>
2022-03-24 11:11:57 +0100

    Sec3_Chap16-Configuring users: added a test

---
+++ section3/springsecuritysection3/src/test/java/com/eazybytes/springsecuritysection3/controllers/BalanceControllerTest.java
@@ -0,0 +1,72 @@
+package com.eazybytes.springsecuritysection3.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = {BalanceController.class})
+class BalanceControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getBalanceDetailsWithBasicAuthenticationOK() throws Exception {
+        mockMvc.perform(get("/myBalance").with(httpBasic("Pierrot","Pierrot123")))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Here are the balance details from the DB"))
+                .andDo(print());
+    }
+
+    @WithMockUser
+    @Test
+    void getBalanceDetailsWithMockUserOK() throws Exception {
+        mockMvc.perform(get("/myBalance").with(user("MockUser")))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Here are the balance details from the DB"))
+                .andDo(print());
+    }
+
+    @WithMockUser(username = "MockUser")
+    @Test
+    void getBalanceDetailsWithMockUserInAnnotationOK() throws Exception {
+        mockMvc.perform(get("/myBalance"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Here are the balance details from the DB"))
+                .andDo(print());
+    }
+
+    @WithMockUser
+    @Test
+    void getBalanceDetailsWithMockUserSimple() throws Exception {
+        mockMvc.perform(get("/myBalance"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Here are the balance details from the DB"))
+                .andDo(print());
+    }
+
+    @Test
+    void getBalanceDetailsWithFormloginOK() throws Exception {
+        FormLoginRequestBuilder formLoginRequestBuilder = formLogin().user("Pierrot").password("Pierrot123");
+        mockMvc.perform(formLoginRequestBuilder)
+                .andExpect(authenticated())
+                .andDo(print());
+    }
+}
