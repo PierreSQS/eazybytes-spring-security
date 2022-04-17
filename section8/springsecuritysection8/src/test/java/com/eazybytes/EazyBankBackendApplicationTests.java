0944b3363c82be07bfab738784a24d0ef8358555
PierreSQS <nomo_messina@web.de>
2022-04-18 00:39:12 +0200

    Sec8_Chap70-Adding a custom: refactored code as recommended by Spring Team

--- section8/springsecuritysection8/src/test/java/com/eazybytes/EazyBankBackendApplicationTests.java
+++ section8/springsecuritysection8/src/test/java/com/eazybytes/EazyBankBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eazybytes.application;
+package com.eazybytes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- section8/springsecuritysection8/src/test/java/com/eazybytes/controller/ContactControllerIT.java
+++ section8/springsecuritysection8/src/test/java/com/eazybytes/controller/ContactControllerIT.java
@@ -1,26 +1,24 @@
 package com.eazybytes.controller;
 
 import com.eazybytes.model.Contact;
-import com.eazybytes.repository.ContactRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = {ContactController.class})
-class ContactControllerTest {
+@WebMvcTest(ContactController.class)
+@AutoConfigureDataJpa
+class ContactControllerIT {
 
     @Autowired
     MockMvc mockMvc;
@@ -28,26 +26,15 @@ class ContactControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockBean
-    ContactRepository contactMockRepo;
-
-    Contact contactMock;
-    Contact savedContactMock;
+    Contact contact;
 
     @BeforeEach
     void setUp() {
-        contactMock = new Contact();
-        contactMock.setContactEmail("pierrottest@example.com");
-        contactMock.setContactName("Pierrot Test");
-        contactMock.setMessage("Please contact me");
-        contactMock.setSubject("My Credit Card");
-
-        savedContactMock = new Contact();
-        savedContactMock.setContactId("SR121231313");
-        savedContactMock.setContactEmail("pierrottest@example.com");
-        savedContactMock.setContactName("Pierrot Test");
-        savedContactMock.setMessage("Please contact me");
-        savedContactMock.setSubject("My Credit Card");
+        contact = new Contact();
+        contact.setContactEmail("pierrottest@example.com");
+        contact.setContactName("Pierrot Test");
+        contact.setMessage("Please contact me");
+        contact.setSubject("My Credit Card");
 
     }
 
@@ -55,8 +42,7 @@ class ContactControllerTest {
     void saveContactInquiryDetails() throws Exception {
 
         // Given
-        String contactAsString = objectMapper.writeValueAsString(contactMock);
-        given(contactMockRepo.save(any())).willReturn(savedContactMock);
+        String contactAsString = objectMapper.writeValueAsString(contact);
 
         // When , then
         mockMvc.perform(post("/contact")
