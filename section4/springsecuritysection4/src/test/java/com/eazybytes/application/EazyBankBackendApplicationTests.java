a3a26fb55d7f5ee60752db0c9c4d3606e5871872
PierreSQS <nomo_messina@web.de>
2022-04-23 11:29:25 +0200

    Sec4_Chap37-Implementing and Enhancing: added an assertion

--- section4/springsecuritysection4/src/test/java/com/eazybytes/application/EazyBankBackendApplicationTests.java
+++ section4/springsecuritysection4/src/test/java/com/eazybytes/application/EazyBankBackendApplicationTests.java
@@ -2,11 +2,14 @@ package com.eazybytes.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class EazyBankBackendApplicationTests {
 
 	@Test
-	void contextLoads() {}
+	void contextLoads(ApplicationContext appCtx) {assertThat(appCtx).isNotNull();}
 
 }
