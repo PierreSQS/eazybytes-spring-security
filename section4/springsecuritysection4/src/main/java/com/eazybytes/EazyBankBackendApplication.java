9c41868fac0e15b318aaf81bbb560f1039ba881a
PierreSQS <nomo_messina@web.de>
2022-04-23 11:57:51 +0200

    Sec4_Chap37-Implementing and Enhancing: added a CLR-Bean

--- section4/springsecuritysection4/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ section4/springsecuritysection4/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -1,13 +1,26 @@
 package com.eazybytes;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EazyBankBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EazyBankBackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run(ApplicationContext appCtx){
+		return args -> Arrays.stream(appCtx.getBeanDefinitionNames())
+				.filter(name -> name.contains("eazy"))
+				.sorted()
+				.forEach(System.out::println);
 	}
 
 }
