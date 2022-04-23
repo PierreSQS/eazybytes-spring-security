0145d482e858b4a3934fba436e95d5edb9e19b1c
PierreSQS <nomo_messina@web.de>
2022-04-23 11:30:38 +0200

    Sec4_Chap37-Implementing and Enhancing: optimized Code

--- section4/springsecuritysection4/src/main/java/com/eazybytes/application/EazyBankBackendApplication.java
+++ section4/springsecuritysection4/src/main/java/com/eazybytes/application/EazyBankBackendApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScans({ @ComponentScan("com.eazybytes.controller"), @ComponentScan("com.eazybytes.config")})
+@ComponentScan(basePackages = {"com.eazybytes.controller", "com.eazybytes.config"})
 @EnableJpaRepositories("com.eazybytes.repository")
 @EntityScan("com.eazybytes.model")
 public class EazyBankBackendApplication {
