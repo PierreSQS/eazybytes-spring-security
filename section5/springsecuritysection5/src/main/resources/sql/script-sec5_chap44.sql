d7bb778ab03d4e972227a0b06eb135056f649c31
PierreSQS <nomo_messina@web.de>
2022-04-24 23:33:15 +0200

    Sec5_Chap44-Implementing and Customizing: EazyBytes Code

---
+++ section5/springsecuritysection5/src/main/resources/sql/script-sec5_chap44.sql
@@ -0,0 +1,39 @@
+-- THIS DB-SCHEMA IN ALSO USED IN SECTION 3 !!!!!!!!!!!!
+DROP DATABASE IF EXISTS eazybytessection3db;
+CREATE DATABASE eazybytessection3db;
+
+USE eazybytessection3db;
+
+-- THE TABLE AS PER REQUEST IN SECT3-CHAP25 (JdbUserDetailsManager)!!!!!!!!!
+-- BEGIN
+CREATE TABLE `users` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`username` VARCHAR(45) NOT NULL,
+`password` VARCHAR(100) NOT NULL,
+`enabled` INT NOT NULL,
+PRIMARY KEY (`id`));
+
+CREATE TABLE `authorities` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `authority` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+INSERT IGNORE INTO `users` VALUES (NULL, 'happy', '$2a$10$HsfhAD4D6dlyDkahIyS.JOjyVOqTLkpD0FPDH//DVMlbS9hhKo1bG', '1');#12345
+INSERT IGNORE INTO `authorities` VALUES (NULL, 'happy', 'write');
+-- END SECT3-CHAP25 (JdbUserDetailsManager)
+
+-- THE ADDITIVE TABLE AS PER REQUEST IN SECT3-CHAP27 (Custom UserDetailsManager)!!!!!!!!!
+CREATE TABLE `customer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `pwd` varchar(100) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `customer` (`email`, `pwd`, `role`)
+ VALUES ('johndoe@example.com', '$2a$10$eoZ9eJcnNYCKs1fxShfsvO6uVIUJ14a5vDi/JQTXrm8AkCy9t0eGy', 'admin');#54321
+
+INSERT INTO `customer` (`email`, `pwd`, `role`)
+ VALUES ('happy@example.com', '$2y$12$oRRbkNfwuR8ug4MlzH5FOeui.//1mkd.RsOAJMbykTSupVy.x/vb2', 'admin');#54321
