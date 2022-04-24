fa08b2cec6034019ce58ab4d0e85b74d7df571fd
PierreSQS <nomo_messina@web.de>
2022-04-25 01:10:58 +0200

    Sec5_Chap44-Implementing and Customizing: associated code to chap

--- section5/springsecuritysection5/src/main/java/com/eazybytes/config/EazyBankUsernamePwdAuthenticationProvider.java
+++ section5/springsecuritysection5/src/main/java/com/eazybytes/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -1,50 +1,64 @@
 package com.eazybytes.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.eazybytes.model.Customer;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import com.eazybytes.model.Customer;
 import com.eazybytes.repository.CustomerRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
 
-	@Autowired
-	private CustomerRepository customerRepository;
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+	private final CustomerRepository customerRepository;
 
-	@Override
-	public Authentication authenticate(Authentication authentication) {
-		String username = authentication.getName();
-		String pwd = authentication.getCredentials().toString();
-		List<Customer> customer = customerRepository.findByEmail(username);
-		if (customer.size() > 0) {
-			if (passwordEncoder.matches(pwd, customer.get(0).getPwd())) {
-				List<GrantedAuthority> authorities = new ArrayList<>();
-				authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
-				return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
-			} else {
-				throw new BadCredentialsException("Invalid password!");
-			}
-		}else {
-			throw new BadCredentialsException("No user registered with this details!");
-		}
+	private final PasswordEncoder passwordEncoder;
+
+	public EazyBankUsernamePwdAuthenticationProvider(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+		this.customerRepository = customerRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
-	public boolean supports(Class<?> authenticationType) {
-		return authenticationType.equals(UsernamePasswordAuthenticationToken.class);
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		// the Authentication Object to Render
+		UsernamePasswordAuthenticationToken usernamePwdAuth;
+		// get Login User Credentials
+		String username = authentication.getName();
+		String pwd = authentication.getCredentials().toString();
+
+		// get User Credentials from DB
+		Optional<Customer> customerFromDBOpt = customerRepository.findByEmail(username);
+
+		// validate Login User Credentials or not by comparing with Credentials from DB
+		if (customerFromDBOpt.isPresent()) {
+			Customer customer = customerFromDBOpt.get();
+			if (passwordEncoder.matches(pwd,customer.getPwd())) {
+				List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+				usernamePwdAuth = new UsernamePasswordAuthenticationToken(username,pwd, authorities);
+			} else {
+				throw new BadCredentialsException("Invalid Password!!!!");
+			}
+
+		} else {
+			throw new BadCredentialsException("No user registered with this details!: "+username);
+		}
+
+		// return the Authentication Token
+		return usernamePwdAuth;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
 	}
 }
