b2dab3f894efdda8b30dc302a19ce0fec4f8c097
PierreSQS <nomo_messina@web.de>
2022-05-11 00:12:30 +0200

    Sec9_Chap77-Making project: preparing code for JWT

--- section9/springsecuritysection9/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ section9/springsecuritysection9/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -6,40 +6,44 @@ import com.eazybytes.filter.RequestValidationBeforeFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 //@EnableWebSecurity(debug = true)
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	/**
-	 * /myAccount - Secured /myBalance - Secured /myLoans - Secured /myCards -
-	 * Secured /notices - Not Secured /contact - Not Secured
+	 * /myAccount - Secured
+	 * /myBalance - Secured
+	 * /myLoans - Secured
+	 * /myCards - Secured
+	 * /notices - Not Secured
+	 * /contact - Not Secured
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.cors().configurationSource(request ->  {
 				CorsConfiguration config = new CorsConfiguration();
 				config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
 				config.setAllowedMethods(Collections.singletonList("*"));
 				config.setAllowCredentials(true);
 				config.setAllowedHeaders(Collections.singletonList("*"));
+				config.setExposedHeaders(List.of("Authorization"));
 				config.setMaxAge(3600L);
 				return config;
 			})
 				.and()
-					.csrf().ignoringAntMatchers("/contact").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-				.and().
-					addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
+					.csrf().disable()
+					.addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
 					.addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
 					.addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
 				.authorizeRequests()
---
+++ section9/springsecuritysection9/src/main/java/com/eazybytes/constants/SecurityConstants.java
@@ -0,0 +1,10 @@
+package com.eazybytes.constants;
+
+public class SecurityConstants {
+
+    private SecurityConstants() {
+    }
+
+    public static final String JWT_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
+    public static final String JWT_HEADER = "Authorization";
+}
